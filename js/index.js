a0cb60fa553a813b6380727c32923dc65304024b
ice949 <justineimasiku21@gmail.com>
2022-06-28 18:16:21 +0200

    Add id to each book added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-// const { object } = require("webidl-conversions");
-
-// console.log('Index started');
-
 const Books = [];
 const BookList = [];
 
@@ -17,17 +13,33 @@ function callbackFunction(event) {
     const myFormData = new FormData(event.target);
 
     const formDataObj = {
+        id: BookList.length,
         title: bookTitle.value,
         author: bookAuthor.value,
     };
     BookList.push(formDataObj); 
     let list = document.createElement("li");
     list.classList.add('list')
-    list.innerHTML = `<p>${formDataObj['title']}</p>
+    list.id = BookList.length-1;
+    list.innerHTML = `
+    <p class="book-id">${formDataObj.id}</p>
+    <p>${formDataObj['title']}</p>
     <p>${formDataObj['author']}</p>
-    <button type="button" class="${BookList.length}">Remove</button>`
+    <button type="button" id="${BookList.length-1}" class="remove">Remove</button>
+    <br> <hr />`
     booklist.appendChild(list)                  
     myFormData.forEach((value, key) => (formDataObj[key] = value));
     console.log(formDataObj);
     console.log(BookList);
 };
+
+const removeBtn = document.querySelector('.remove')
+removeBtn.addEventListener('click', ()=> {
+    if ()
+})
+
+
+function removeObject(book) {
+    BookList.splice(book.id, 1);
+}
+
