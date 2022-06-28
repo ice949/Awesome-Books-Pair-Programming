a4407022513291f87b0c60f590d94633e74d8d4c
Fourteen98 <emuhiydeen6@gmail.com>
2022-06-28 15:56:23 +0000

    add css file

--- js/index.js
+++ js/index.js
@@ -11,51 +11,6 @@ const bookAuthor = document.getElementById('author');
 const form = document.getElementById('form');
 const booklist = document.querySelector(".book-list")
 
-// let formData = {
-//     title: "",
-//     author: "",
-// };
-
-// function handleChange() {
-//     formData.title = bookTitle.value;
-//     formData.author = bookAuthor.value;
-//     localStorage.setItem('form', JSON.stringify(formData));
-// }
-
-// let formData = {
-//     title: "",
-//     author: "",
-// };
-
-// form.addEventListener('submit', () => {
-//     const getFormValue = localStorage.getItem('form');
-    
-//     if (getFormValue) {
-//         const formObject = JSON.parse(getFormValue);
-//         bookTitle.value = formObject.title;
-//         bookAuthor.value = formObject.author;
-//     }
-
-//     let titleName = getFormValue['title'];
-//     let authorName = getFormValue['author'];
-//     console.log(titleName + authorName);
-//     console.log(formData);
-//     Books.push(getFormValue);
-//     localStorage.setItem('formd', JSON.stringify(getFormValue));
-//     localStorage.getItem('formd');
-//     console.log(getFormValue);
-//     console.log(Books);
-// });
-
-// bookTitle.onkeyup = handleChange;
-// bookAuthor.onkeyup = handleChange;
-
-// form.addEventListener('submit', ()=> {
-//     // const book = object.create(formData)
-
-//     console.log(Books);
-// });
-
 form.addEventListener('submit', callbackFunction);
 function callbackFunction(event) {
     event.preventDefault();
