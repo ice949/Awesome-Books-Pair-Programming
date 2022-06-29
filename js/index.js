f284712de705b5f56974d004c8f0813f8bb16bb8
Fourteen98 <emuhiydeen6@gmail.com>
2022-06-29 22:37:11 +0000

    add fix for button id

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const Books = [];
-const BookList = [];
+var BookList = [];
 
 
 const bookTitle = document.getElementById('title');
@@ -17,31 +17,34 @@ function callbackFunction(event) {
         title: bookTitle.value,
         author: bookAuthor.value,
     };
-    BookList.push(formDataObj); 
+    BookList.push(formDataObj);
     let list = document.createElement("li");
     list.classList.add('list')
-    list.id = BookList.length-1;
+    list.id = BookList.length - 1;
     list.innerHTML = `
     <p class="book-id">${formDataObj.id}</p>
     <p id='test'>${formDataObj['title']}</p>
     <p>${formDataObj['author']}</p>
-    <button type="button" id="${BookList.length-1}" class="remove">Remove</button>
-    <br> <hr />`
-    booklist.appendChild(list)                  
+    <button type="button" id="${BookList.length - 1}" class="remove">Remove</button>
+    <br> <hr />
+    `
+    booklist.appendChild(list)
     myFormData.forEach((value, key) => (formDataObj[key] = value));
     console.log(formDataObj);
     console.log(BookList);
 
-    const removeBtn = document.querySelector('.remove');
-    removeBtn.onclick = () => { 
-        BookList.splice(removeBtn.id, 1);
-        console.log(BookList);
-    };
+    const removeBtn = document.querySelectorAll('.remove');
+    // removeBtn.onclick = () => {
+    //     BookList.splice(removeBtn.id, 1);
+    //     console.log(BookList);
+    // };
+    console.log(removeBtn[0].id);
+    for (var i = 0; i < removeBtn.length; i++) {
+
+        removeBtn[i].addEventListener('click', () => {
+            BookList = BookList.splice(removeBtn[i].id, 1);
+            console.log(BookList);
+        })
+    }
+
 };
-
-
-
-// function removeObject(book) {
-//     BookList.splice(book.id, 1);
-// }
-
