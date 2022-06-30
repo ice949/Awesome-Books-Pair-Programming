f9563f85a30dbe7e1b59bad5245d6d5beaec6196
Fourteen98 <emuhiydeen6@gmail.com>
2022-06-30 17:11:00 +0000

    fix issue of remove book by id

--- js/index.js
+++ js/index.js
@@ -1,105 +1,91 @@
-const books = [];
-
-let errorMessage = document.getElementById('error-message');
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
-const bookContainer = document.querySelector(".book-list")
+const bookContainer = document.querySelector('.book-list');
 const form = document.getElementById('form');
+
+// booksArray to store array of object
+let books = [];
 
 // Create a js Object to store form data
 const formData = {
-    // id: '',
-    title: '',
-    author: '',
+  title: '',
+  author: '',
 };
 
+function removeElementFromUI(target) {
+  if (target.classList.contains('remove-btn')) {
+    target.parentElement.remove();
+  }
+}
 
-// Event listner for form on submit event
-form.addEventListener('submit', (e) => {
-    e.preventDefault
-    formData.title = bookTitle.value;
-    formData.author = bookAuthor.value;
-    formData.id = books.length;
-    localStorage.setItem('form', JSON.stringify(formData));
-    pushIntoBook(formData);
-    displayBook(books.length - 1);
-});
-
-
-// event listener for doucment on cotent loaded
-document.addEventListener('DOMContentLoaded', (e) => {
-    if (localStorage.getItem('form')) {
-        const formObj = JSON.parse(localStorage.getItem('form'));
-        bookTitle.value = formObj.title;
-        bookAuthor.value = formObj.author;
-    }
-});
-
-
-// function to push form data object to book array
-function pushIntoBook(formData) {
-    books.push(formData);
+function removeFromBooks(title) {
+  let tempBooks;
+  if (localStorage.getItem('books') === null) {
+    tempBooks = [];
+  } else {
+    tempBooks = JSON.parse(localStorage.getItem('books'));
+    tempBooks.forEach((book, index) => {
+      if (book.title === title) {
+        tempBooks.splice(index, 1);
+        localStorage.setItem('books', JSON.stringify(tempBooks));
+      }
+    });
+  }
 }
 
 // display data in the browser
 function displayBook(index) {
-    const list = document.createElement('div');
-    list.classList.add('single-book');
+  const list = document.createElement('div');
+  list.classList.add('single-book');
 
-    // const id = document.createElement('h3');
-    // id.textContent = books[index].id;
-    // id.classList.add('book-id');
-    // list.appendChild(id);
+  const title = document.createElement('h3');
+  title.textContent = books[index].title;
+  title.classList.add('title');
+  list.appendChild(title);
 
-    const title = document.createElement('h3');
-    title.textContent = books[index].title;
-    title.classList.add('title');
-    list.appendChild(title);
+  const author = document.createElement('h3');
+  author.textContent = books[index].author;
+  author.classList.add('author');
+  list.appendChild(author);
 
-    const author = document.createElement('p');
-    author.textContent = books[index].author;
-    author.classList.add('author');
-    list.appendChild(author);
+  const removeBtn = document.createElement('button');
+  removeBtn.classList.add('remove-btn');
+  removeBtn.textContent = 'Remove';
+  removeBtn.setAttribute('type', 'button');
+  list.appendChild(removeBtn);
 
-    removeBtn = document.createElement('button');
-    removeBtn.classList.add('remove-btn');
-    removeBtn.textContent = 'Remove';
-    list.appendChild(removeBtn);
+  const bar = document.createElement('hr');
+  bar.classList.add('bar');
+  list.appendChild(bar);
 
-    //adding book to ul
-    bookContainer.append(list);
+  // adding book to ul
+  bookContainer.append(list);
 
-    // add event listener to removeBtn
-    list.addEventListener('click', (e) => {
-        // bookContainer.removeChild(list);
-        // books.splice(list.childNodes[0].innerText, 1);
-        removeElementFromUI(e.target);
-        removeElementFromUI(e.target.parentElement.firstChild.textContent);
-        console.log(e.target);
-    });
-    
-    
+  // add event listener to removeBtn
+  list.addEventListener('click', (e) => {
+    // bookContainer.removeChild(list);
+    // books.splice(list.childNodes[0].innerText, 1);
+    removeElementFromUI(e.target);
+    removeFromBooks(e.target.parentElement.firstChild.textContent);
+  });
 }
 
-function removeElementFromUI(target) {
-    if (target.classList.contains('remove-btn')) {
-        target.parentElement.remove();
-        console.log('element with remove-btn class removed');
-    } else {
-        console.log('does not have classlist of remove-btn');
-    }
-}
+// Event listner for form on submit event
+form.addEventListener('submit', (e) => {
+  formData.title = bookTitle.value;
+  formData.author = bookAuthor.value;
+  books.push(formData);
+  displayBook(books.length - 1);
+  e.preventDefault();
+  localStorage.setItem('books', JSON.stringify(books));
+  if (localStorage.getItem('books')) {
+    books = JSON.parse(localStorage.getItem('books'));
+  }
+});
 
-function removeFromBooks(title) {
-    let tempBooks = [];
-    if (localStorage.getItem('form')) {
-        tempBooks = JSON.parse(localStorage.getItem('form'));
-    }
-
-    tempBooks.forEach((book, index) => {
-        if (book.title === title) {
-            tempBooks.splice(index, 1);
-            console.log(tempBooks);
-        }
-    });
-}
+// event listener for doucment on cotent loaded
+document.addEventListener('DomContentLoaded', () => {
+  if (localStorage.getItem('books')) {
+    books = JSON.parse(localStorage.getItem('books'));
+  }
+});
