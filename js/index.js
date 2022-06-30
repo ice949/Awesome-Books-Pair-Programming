afa52a930b79ed739dfc8add5697259132c938dd
ice949 <justineimasiku21@gmail.com>
2022-06-30 23:38:20 +0200

    Add books to UI

--- js/index.js
+++ js/index.js
@@ -39,6 +39,41 @@ class LocalStorage {
   }
 }
 
+class CreateUI{
+  getUI(localStorage){
+    this.books = localStorage.getLocalStorage();
+  }
+
+  createUI(books){
+    const list = document.createElement("div");
+    list.classList.add("single-book");
+
+    const title = document.createElement("h3");
+    title.textContent = book.title;
+    title.classList.add("title");
+    list.appendChild(title);
+
+    const author = document.createElement("h3");
+    author.textContent = book.author;
+    author.classList.add("author");
+    list.appendChild(author);
+
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("remove-btn");
+    removeBtn.textContent = "Remove";
+    removeBtn.setAttribute("type", "button");
+    list.appendChild(removeBtn);
+
+    const bar = document.createElement("hr");
+    bar.classList.add("bar");
+    list.appendChild(bar);
+
+    // adding book to ul
+    bookContainer.append(list);
+
+  }
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const book = new AwesomeBooks(bookTitle.value, bookAuthor.value);
