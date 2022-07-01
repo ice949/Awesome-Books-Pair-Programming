3b391e43952df1c59048b7c5a10dcdacc475c67f
ice949 <justineimasiku21@gmail.com>
2022-07-01 18:42:53 +0200

    Add navigation to pages

--- js/index.js
+++ js/index.js
@@ -140,3 +140,30 @@ window.addEventListener('load', () => {
   // set the id var to current index value before refresh
   id = 1 + getIndexInLocalStorage();
 });
+
+
+// Navigation
+const addBookNav = document.getElementById('add-page-btn');
+const booksNav = document.getElementById('books-page-btn');
+const contactNav = document.getElementById('contact-page-btn');
+const addPage = document.getElementById('add-page');
+const booksPage = document.getElementById('books-page');
+const contactPage = document.getElementById('contact-page');
+
+addBookNav.addEventListener('click', ()=> {
+  addPage.classList.remove('none');
+  booksPage.classList.add('none');
+  contactPage.classList.add('none');
+});
+
+booksNav.addEventListener('click', ()=> {
+  addPage.classList.add('none');
+  booksPage.classList.remove('none');
+  contactPage.classList.add('none');
+});
+
+contactNav.addEventListener('click', ()=> {
+  addPage.classList.add('none');
+  booksPage.classList.add('none');
+  contactPage.classList.remove('none');
+})
